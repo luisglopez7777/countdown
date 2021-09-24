18d6de2c0de187e83e56b4d2a95de97674d40fbc
Luis <luisglopez7777@gmail.com>
2021-09-24 00:28:39 -0500

    countdown working with daet-fns and hooks

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Countdown from './components/Countdown';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header className="App-header">
         <h1>Countdown</h1>
       </header>
-      <div className="content">
-        <Countdown date='2020-12-24T00:00:00' />,
-      </div>
-    </div>
+      <Countdown />
+    </>
   );
 }
 
